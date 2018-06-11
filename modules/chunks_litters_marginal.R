4f704fdfc697c802cf32d5c414f1551822b8b982
Christopher Paciorek <paciorek@scf.Berkeley.EDU>
2018-06-10 17:23:55 -0700

    worked through parts of session 4

--- modules/chunks_litters_marginal.R
+++ modules/chunks_litters_marginal.R
@@ -36,8 +36,23 @@ littersMargCode <- nimbleCode({
 
 ## @knitr littersMarg-model
 
+G <- 2
+N <- 16
+n <- matrix(c(13, 12, 12, 11, 9, 10, 
+              9, 9, 8, 11, 8, 10, 13, 10, 12, 9, 10, 9, 10, 5, 9, 9, 13, 
+              7, 5, 10, 7, 6, 10, 10, 10, 7), nrow = 2)
+r <- matrix(c(13, 12, 12, 11, 9, 10, 9, 9, 8, 10, 8, 9, 
+     12, 9, 11, 8, 9, 8, 9, 4, 8, 7, 11, 4, 4, 5, 5, 3, 7, 3, 7, 0), 
+     nrow = 2)
+              
+littersConsts <- list(G = G, N = N, n = n)
+littersData <- list(r = r)
+littersInits <- list( a = c(2, 2), b=c(2, 2) )
+
 littersMargModel <- nimbleModel(littersMargCode, 
           data = littersData, constants = littersConsts, inits = littersInits)
+
+## @knitr littersMarg-model-compile
 
 cLittersMargModel <- compileNimble(littersMargModel)
 
